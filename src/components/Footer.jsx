3f0bfa55d46b1a36e2780e259130dff07b0c23ed
MrTban <esteman214@gmail.com>
2023-05-19 10:13:52 -0500

    create conditional render filters in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,21 @@
-export function Footer() {
+import { IS_DEVELOPMENT } from '../config'
+
+export function Footer({ filters }) {
 	return (
-		<footer>
-			<h4>
-				Technical test in React ⚛️ － <span>@mrtban.dev</span>
-			</h4>
-			<h5>Shoppin Cart with useContext & useReducer</h5>
+		<footer className='fixed left-4 bottom-4 text-left bg-black/70 px-2 py-6 rounded-3xl opacity-95 backdrop-blur'>
+			{IS_DEVELOPMENT ? (
+				JSON.stringify(filters, null, 2)
+			) : (
+				<>
+					<h4 className='flex m-0'>
+						Technical test in React ⚛️ －{' '}
+						<span className='text-sm text-[#09f] opacity-80 flex items-center'>
+							@mrtban.dev
+						</span>
+					</h4>
+					<h5 className='flex m-0'>Shoppin Cart with useContext & useReducer</h5>
+				</>
+			)}
 		</footer>
 	)
 }

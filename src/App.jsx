7841d0c09f2e1d5843c1c3ce14b4037ccb43dbdb
MrTban <esteman214@gmail.com>
2023-05-19 11:34:51 -0500

    add useFilters and revome props

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useContext, useState } from 'react'
 import { products as initialProducts } from './mocks/products.json'
+import { useState } from 'react'
+import { useFilters } from './hooks/useFilters'
 
 import { Products } from './components/Products'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
-import { FiltersContext } from './context/filters'
-
-function useFilters() {
-	const { filters, setFilters } = useContext(FiltersContext)
-
-	const filterProducts = (products) => {
-		return products.filter((product) => {
-			return (
-				product.price >= filters.minPrice &&
-				(filters.category === 'all' || product.category === filters.category)
-			)
-		})
-	}
-
-	return { filters, filterProducts, setFilters }
-}
 
 function App() {
 	const [products] = useState(initialProducts)
-	const { filters, filterProducts, setFilters } = useFilters()
+	const { filters, filterProducts } = useFilters()
 
 	const filteredProducts = filterProducts(products)
 
 	return (
 		<>
-			<Header changeFilters={setFilters} />
+			<Header />
 			<Products products={filteredProducts} />
 			<Footer filters={filters} />
 		</>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import { Filters } from './Filters'
 
-export function Header({ changeFilters }) {
+export function Header() {
 	return (
 		<header>
 			<h1 className='text-2xl font-semibold mb-6'>React Shop 🛒</h1>
-			<Filters onChange={changeFilters} />
+			<Filters />
 		</header>
 	)
 }

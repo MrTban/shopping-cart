d12755e7d4d3e828a487e6e2e23e0b0382eac5ae
MrTban <esteman214@gmail.com>
2023-05-19 11:34:37 -0500

    useId and delete prop driling

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,18 +1,24 @@
 import { useState } from 'react'
+import { useFilters } from '../hooks/useFilters'
+import { useId } from 'react'
 
-export function Filters({ onChange }) {
+export function Filters() {
+	const { setFilters } = useFilters()
+
 	const [minPrice, setMinPrice] = useState(0)
+	const minPriceFilterId = useId()
+	const categoryFilterId = useId()
 
 	const handleChangeMinPrice = (event) => {
 		setMinPrice(event.target.value)
-		onChange((prevState) => ({
+		setFilters((prevState) => ({
 			...prevState,
 			minPrice: event.target.value,
 		}))
 	}
 
 	const handleChangeCategory = (event) => {
-		onChange((prevState) => ({
+		setFilters((prevState) => ({
 			...prevState,
 			category: event.target.value,
 		}))
@@ -21,10 +27,10 @@ export function Filters({ onChange }) {
 	return (
 		<section className='flex flex-col gap-4 sm:flex-row items-center justify-between text-sm font-bold mb-4'>
 			<div className='flex gap-4'>
-				<label htmlFor='price'>Price starting at:</label>
+				<label htmlFor={minPriceFilterId}>Price starting at:</label>
 				<input
 					type='range'
-					id='price'
+					id={minPriceFilterId}
 					min='0'
 					max='1000'
 					onChange={handleChangeMinPrice}
@@ -33,8 +39,8 @@ export function Filters({ onChange }) {
 			</div>
 
 			<div className='flex gap-4'>
-				<label htmlFor='category'>Category</label>
-				<select id='category' onChange={handleChangeCategory}>
+				<label htmlFor={categoryFilterId}>Category</label>
+				<select id={categoryFilterId} onChange={handleChangeCategory}>
 					<option value='all'>All</option>
 					<option value='laptops'>Laptops</option>
 					<option value='smartphones'>Smartphones</option>
